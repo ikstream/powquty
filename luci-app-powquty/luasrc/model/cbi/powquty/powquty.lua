a0ae76a063af25aef8aa53c3b055be4b1c20779c
Stefan Venz <stefan.venz@protonmail.com>
2017-05-28 23:42:54 +0200

    refactor namedsection
    Signed-off-by: Stefan Venz <stefan.venz@protonmail.com>

--- luci-app-powquty/luasrc/model/cbi/powquty/powquty.lua
+++ luci-app-powquty/luasrc/model/cbi/powquty/powquty.lua
@@ -1,6 +1,5 @@
 
 m = Map("powquty", "General Configuration")
-m:chain("powquty_event_log")
 s = m:section(NamedSection, "powquty", "powquty", "Configuration")
 
 device_tty = s:option(Value, "device_tty", "Device tty", "The path to the tty device created by cdc-acm driver")
@@ -32,49 +31,50 @@ max_log_size_kb.datatype = "string"
 max_log_size_kb.default = "4096"
 
 
+m:chain("powquty_event_log")
 
 m1 = Map("powquty_event_log", "Powquty Event Log")
-sl = m1:section(NamedSection, "powquty_event_log", "powquty_event_log", "Event Log Configuration")
+s1 = m:section(NamedSection, "powquty_event_log", "powquty event log", "Event Log Configuration")
 
-powquty_email = sl:option(Flag, "powquty_email", "powquty_email", "If activated, powquty will send notifications via email")
+powquty_email = s1:option(Flag, "powquty_email", "powquty_email", "If activated, powquty will send notifications via email")
 powquty_email.rmempty = false
 powquty_email.default = false
 
-powquty_adress = sl:option(Value, "powquty_adress", "powquty_adress", "Email adress to use")
+powquty_adress = s1:option(Value, "powquty_adress", "powquty_adress", "Email adress to use")
 powquty_adress.datatype = "String"
 powquty_adress.default = ""
 
-powquty_subject = sl:option(Value, "powquty_subject", "powquty_subject", "Subject prefix")
+powquty_subject = s1:option(Value, "powquty_subject", "powquty_subject", "Subject prefix")
 powquty_subject.datatype = "String"
 powquty_subject.default = "[EN50160-Event]"
 
-mqtt_eventflag = sl:option(Flag, "mqtt_eventflag", "mqtt_eventflag", "If activated, powquty will send mqtt notification on EN50160 Event")
+mqtt_eventflag = s1:option(Flag, "mqtt_eventflag", "mqtt_eventflag", "If activated, powquty will send mqtt notification on EN50160 Event")
 mqtt_eventflag.rmempty = false
 mqtt_eventflag.default = true
 
-mqtt_eventhost = sl:option(Value, "mqtt_eventhost", "mqtt_eventhost", "IP-address or URL to the MQTT broker who receives the publish messages of powqutd")
+mqtt_eventhost = s1:option(Value, "mqtt_eventhost", "mqtt_eventhost", "IP-address or URL to the MQTT broker who receives the publish messages of powqutd")
 mqtt_eventhost.datatype = "string"
 mqtt_eventhost.default = "localhost"
 
-mqtt_eventtopic = sl:option(Value, "mqtt_eventtopic", "mqtt_eventtopic", "The topic powquty publishes EN50160 events to")
+mqtt_eventtopic = s1:option(Value, "mqtt_eventtopic", "mqtt_eventtopic", "The topic powquty publishes EN50160 events to")
 mqtt_eventtopic.datatype = "String"
 mqtt_eventtopic.default = "device/en50160-event"
 
-powquty_slack = sl:option(Flag, "powquty_slack", "powquty_slack", "If activated, powquty will send notifications to a slack channel")
+powquty_slack = s1:option(Flag, "powquty_slack", "powquty_slack", "If activated, powquty will send notifications to a slack channel")
 powquty_slack.rmempty = false
 powquty_slack.default = false
 
-slack_webhook = sl:option(Value, "slack_webhook", "slack_webhook", "Incoming webhook for slack")
+slack_webhook = s1:option(Value, "slack_webhook", "slack_webhook", "Incoming webhook for slack")
 slack_webhook.datatype = "string"
 slack_webhook.default = ""
 
-slack_user = sl:option(Value, "slack_user", "slack_user", "The user name to use for messages")
+slack_user = s1:option(Value, "slack_user", "slack_user", "The user name to use for messages")
 slack_user.datatype = "string"
 slack_user.default = "PowQuty_novio"
 
-slack_channel = sl:option(Value, "slack_channel", "slack_channel", "Post to this channel")
+slack_channel = s1:option(Value, "slack_channel", "slack_channel", "Post to this channel")
 slack_channel.datatype = "string"
 slack_channel.default = "#general"
 
-return m,m1
+return m1,m
 
